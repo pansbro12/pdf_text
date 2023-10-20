120bdb533fab479c4ecc4c25bb3b2f6c9bf96f2d
patrick ansbro <pansbro@me.com>
2023-10-20 21:08:15 +0100

    fix: git fuck up

--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,6 @@ use pdf_render::{
 pub mod entry;
 mod text;
 mod tree;
-<<<<<<< HEAD
 pub mod util;
 
 pub fn run<B: Backend>(
@@ -24,13 +23,6 @@ pub fn run<B: Backend>(
     page: &Page,
     resolve: &impl Resolve,
 ) -> Result<Flow, PdfError> {
-=======
-mod util;
-mod text;
-pub mod entry;
-
-pub fn run<B: Backend>(file: &pdf::file::CachedFile<B>, page: &Page, resolve: &impl Resolve) -> Result<Flow, PdfError> {
->>>>>>> b61e68d (update pdf)
     let cache = TraceCache::new();
 
     let mut clip_paths = vec![];
@@ -66,7 +58,6 @@ pub fn run<B: Backend>(file: &pdf::file::CachedFile<B>, page: &Page, resolve: &i
 
     let mut spans = vec![];
     let mut lines = vec![];
-<<<<<<< HEAD
 
     let mut visit_item = |item| match item {
         DrawItem::Text(t, _) if bbox.intersects(t.rect) => {
@@ -84,29 +75,6 @@ pub fn run<B: Backend>(file: &pdf::file::CachedFile<B>, page: &Page, resolve: &i
                             segment.baseline.to_y(),
                         ]),
                         _ => {}
-=======
-    let mut visit_item = |item| {
-        match item {
-            DrawItem::Text(t) if bbox.intersects(t.rect) => {
-
-                if t.text.chars().any(|c| !c.is_whitespace()) {
-                    spans.push(t);
-                }
-            }
-            DrawItem::Vector(path) if bbox.intersects(path.outline.bounds()) => {
-                for contour in path.outline.contours() {
-                    use pathfinder_content::{outline::ContourIterFlags, segment::SegmentKind};
-                    for segment in contour.iter(ContourIterFlags::empty()) {
-                        match segment.kind {
-                            SegmentKind::Line => lines.push([
-                                segment.baseline.from_x(),
-                                segment.baseline.from_y(),
-                                segment.baseline.to_x(),
-                                segment.baseline.to_y()
-                            ]),
-                            _ => {}
-                        }
->>>>>>> b61e68d (update pdf)
                     }
                 }
             }
@@ -122,16 +90,10 @@ pub fn run<B: Backend>(file: &pdf::file::CachedFile<B>, page: &Page, resolve: &i
                 continue;
             }
         };
-<<<<<<< HEAD
         let mut pat_tracer = Tracer::new(&cache, &mut clip_paths);
 
         render_pattern(&mut pat_tracer, &*pattern, resolve)?;
 
-=======
-        let mut pat_tracer = Tracer::new(&cache);
-
-        render_pattern(&mut pat_tracer, &*pattern, resolve)?;
->>>>>>> b61e68d (update pdf)
         let pat_items = pat_tracer.finish();
         for item in pat_items {
             visit_item(item);
@@ -141,14 +103,10 @@ pub fn run<B: Backend>(file: &pdf::file::CachedFile<B>, page: &Page, resolve: &i
     for item in items {
         visit_item(item);
     }
-<<<<<<< HEAD
-    //creates a root node for the page
-    let root = tree::build(&spans, bbox, &lines);
-=======
->>>>>>> b61e68d (update pdf)
 
     let root = tree::build(&spans, bbox, &lines);
     let mut flow = Flow::new();
     tree::items(&mut flow, &spans, &root, bbox.min_x());
     Ok(flow)
 }
+
